9a2132565677eadf240930c9f0f9f7080322e56c
in3166 <in3166@naver.com>
2023-01-17 22:53:32 +0900

    Add: React lazy in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,37 +1,42 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { PrivateRoute } from './PrivateRoute';
 import Layout from 'components/Layout';
-import Home from './Home';
-import Login from './Login';
-import Search from './Search';
+import Loading from 'components/Loading';
 import styles from './routes.module.scss';
 import NotFound from './NotFound';
-import { PrivateRoute } from './PrivateRoute';
+
+const Home = lazy(() => import('./Home'));
+const Search = lazy(() => import('./Search'));
+const Login = lazy(() => import('./Login'));
 
 const App = () => {
   return (
     <div className={styles.app}>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route
-            path='/'
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path='/search'
-            element={
-              <PrivateRoute>
-                <Search />
-              </PrivateRoute>
-            }
-          />
-        </Route>
-        <Route path='/login' element={<Login />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route
+              path='/'
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path='/search'
+              element={
+                <PrivateRoute>
+                  <Search />
+                </PrivateRoute>
+              }
+            />
+          </Route>
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
